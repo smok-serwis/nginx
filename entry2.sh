dbb836934561db729608c4ffc0a609697ca5caa3
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2018-03-02 21:44:22 +0100

    Update entry2.sh

--- entry2.sh
+++ entry2.sh
@@ -3,14 +3,13 @@
 mkdir -p /etc/nginx/ssl
 
 if [ -d /run/secrets ]; then
-  cp -rf /run/secrets/*.conf /etc/nginx/conf.d/
-  cp -rf /run/secrets/*.key /etc/nginx/ssl/
-  cp -rf /run/secrets/*.crt /etc/nginx/ssl/
-  
   if [ -f /etc/nginx/nginx.conf ]; then
     mv /run/secrets/nginx.conf /etc/nginx/nginx.conf
   fi
+  
+  cp -rf /run/secrets/*.conf /etc/nginx/conf.d/
+  cp -rf /run/secrets/*.key /etc/nginx/ssl/
+  cp -rf /run/secrets/*.crt /etc/nginx/ssl/
 fi
 
 exec "$@"
-
